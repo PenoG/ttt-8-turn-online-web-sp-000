c1df52319873dabc98184b1cf64cbec03bdbf8e8
Mason Phillips <mjphillips2000@gmail.com>
2020-09-30 13:14:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,12 @@ def position_taken?(board, index)
   end
 end
 
-def move
-  
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move (board, index, current_Player = "X")
+  board[index] = current_Player
 end
 
 def turn 
