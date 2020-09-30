72c9051eaf2cd0e15dc816b541e6e44c7dbc7fd8
Mason Phillips <mjphillips2000@gmail.com>
2020-09-30 13:48:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,8 +32,8 @@ def turn(board)
   index = input_to_index(input)
   if input_to_index(input) == valid_move?(board,index)
     move(board,index)
+    display_board(board)
   else
     turn(board)
   end  
-  display_board(board)
 end
