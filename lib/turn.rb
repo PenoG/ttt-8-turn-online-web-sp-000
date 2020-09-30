d3b4af0f5177459496df7206626f1b15baf2e882
Mason Phillips <mjphillips2000@gmail.com>
2020-09-30 13:48:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index) == true
+  index.between?(0,8) && !position_taken?(board, index) == false
 end
 
 def position_taken?(board, index)
